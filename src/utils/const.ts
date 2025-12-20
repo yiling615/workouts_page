66c0771cdfa20fe55147016c17604ca56be95464
Yiling <143258245+yiling615@users.noreply.github.com>
2025-12-20 21:31:54 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -199,7 +199,7 @@ const purple = '#acbcf9';
 const purple2 = 'rgb(127, 34, 254)';
 const veryPeri = 'rgb(105,106,173)'; //长春花蓝
 const red = 'rgb(255,0,0)'; //大红色
-const orange = `rgb(255,128,0)`
+const orange = `rgb(255,69,0)`
 
 // If your map has an offset please change this line
 // issues #92 and #198
