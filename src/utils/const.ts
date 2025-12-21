aec421670f04dd0920d3dae506ba06134ff9eec1
Yiling <143258245+yiling615@users.noreply.github.com>
2025-12-21 13:00:19 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -259,7 +259,7 @@ export const MAP_TILE_STYLE_DARK = 'outdoor-dark';
 // access token. you can apply a new one, it's free.
 // maptiler: Gt5R0jT8tuIYxW6sNrAg | sign up at https://cloud.maptiler.com/auth/widget
 // stadiamaps: 8a769c5a-9125-4936-bdcf-a6b90cb5d0a4 | sign up at https://client.stadiamaps.com/signup/
-export const MAP_TILE_ACCESS_TOKEN = 'EDVasimn3jOguABufKJX';
+export const MAP_TILE_ACCESS_TOKEN = 'r3MY6ofzXPngWYrQBAp3';
 
 export const MAP_TILE_STYLES = {
   maptiler: {
